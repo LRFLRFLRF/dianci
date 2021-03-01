810719c92243bbd282c7817dc1356e0a0b4dc8cc
LRFLRFLRF <1696799188@qq.com>
2021-03-01 16:07:43 +0800

    添加了fft变换代码

--- mat/fft_test.m
+++ mat/fft_test.m
@@ -1,32 +1,31 @@
 clc;
 clear;
-load('F:\Desktop\j_signal.mat')
-x=VarName3;
-mean = mean(x(:));
-x = x - mean;
-N = size(x);
-n = 0:1:N(1,1)-1;
-figure(1);
-Xk=fft(x,N);      % 傅立叶变换
-subplot(311);
-plot(n,x);hold on;
-title('原信号');
 
-subplot(312);
-plot(n,abs(Xk));
-title('FFT变换')
+yuan_data_path = 'E:\Desktop\dianci\Python_code\mat\';  %106to114_oneDim 文件路径
+yuan_data_name = '106to114';
+
+%% 加载原始波形
+dat = load([yuan_data_path, yuan_data_name, '.mat']);   
+fieldname = fieldnames(dat);   %获取字段名
+name = fieldname{1};
+data_yuan = getfield(dat, name);    %根据字段名读取数据
+
+%% fft变换
+Fs = 48;            % Sampling frequency   单位：HZ                 
+T = 1/Fs;             % Sampling period       
+n = length(data_yuan);             % Length of signal
+
+Y = fft(data_yuan);
 
 
-y1 = ifft(Xk(1:1730),N);
 
-x1 = 1:N;
-% w0 = 2*pi;
-% w1 = 2*pi/15;
-% y1 = zeros(1,length(x1));
-% for i=1:1737
-%     y1 = y1 + Xk(i)*1e-2*sin(w0/i*x1);
-% end
+%% 绘图
+Pyy = Y.*conj(Y)/n;
+f = Fs/n*(0:n/32);
+plot(f,Pyy(1:n/32+1))
+title('Power spectral density')
+xlabel('Frequency (Hz)')
 
-%y1 = 1.14*sin(w0*x1)+1.06*sin(w1*x1);
-subplot(311);
-plot(x1,y1);hold on;
+
+
+
