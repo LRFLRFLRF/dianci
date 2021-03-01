2f2e7f2472e87d9726c5ff954af38a0333c4f515
LRFLRFLRF <1696799188@qq.com>
2021-03-01 16:38:23 +0800

    去除直流分量再做fft

--- mat/fft_test.m
+++ mat/fft_test.m
@@ -2,7 +2,7 @@ clc;
 clear;
 
 yuan_data_path = 'E:\Desktop\dianci\Python_code\mat\';  %106to114_oneDim 文件路径
-yuan_data_name = '106to114';
+yuan_data_name = '106to114_oneDim';
 
 %% 加载原始波形
 dat = load([yuan_data_path, yuan_data_name, '.mat']);   
@@ -10,19 +10,22 @@ fieldname = fieldnames(dat);   %获取字段名
 name = fieldname{1};
 data_yuan = getfield(dat, name);    %根据字段名读取数据
 
+
+
 %% fft变换
 Fs = 48;            % Sampling frequency   单位：HZ                 
 T = 1/Fs;             % Sampling period       
 n = length(data_yuan);             % Length of signal
 
-Y = fft(data_yuan);
-
+mean(data_yuan)
+data = data_yuan - mean(data_yuan);   %去除直流分量
+Y = fft(data);
 
 
 %% 绘图
 Pyy = Y.*conj(Y)/n;
-f = Fs/n*(0:n/32);
-plot(f,Pyy(1:n/32+1))
+f = Fs/n*(0:n/16);
+plot(f,Pyy(1:n/16+1))
 title('Power spectral density')
 xlabel('Frequency (Hz)')
 
