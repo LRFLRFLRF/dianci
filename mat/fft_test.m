5e7c1809ea65334e806ccbd01406c8a84cc420c5
LRFLRFLRF <1696799188@qq.com>
2021-03-01 17:10:50 +0800

    修改注释 注明了2维fft原理

--- mat/fft_test.m
+++ mat/fft_test.m
@@ -13,21 +13,22 @@ data_yuan = getfield(dat, name);    %根据字段名读取数据
 
 
 %% fft变换
-Fs = 48;            % Sampling frequency   单位：HZ                 
+Fs = 48*5;            % 采样频率   单位：次/天    6min一个点那一天24h 就是每天48*5的采样频率                 
 T = 1/Fs;             % Sampling period       
 n = length(data_yuan);             % Length of signal
 
-mean(data_yuan)
 data = data_yuan - mean(data_yuan);   %去除直流分量
-Y = fft(data);
+Y = fft(data, n);
 
 
 %% 绘图
+% Y乘Y共轭=a方加b方   复数Y = a + ib
 Pyy = Y.*conj(Y)/n;
-f = Fs/n*(0:n/16);
-plot(f,Pyy(1:n/16+1))
-title('Power spectral density')
-xlabel('Frequency (Hz)')
+%频率序列f  其中（0：n/x）的x控制plot显示频率范围      
+f = Fs/n*(0:n/12);
+plot(f,Pyy(1:n/12+1))
+title('频谱强度')
+xlabel('频率 (次/天)')
 
 
 
