d3c6bb6f0b4dfeefb903762af113813abd19ca66
LRFLRFLRF <1696799188@qq.com>
2021-08-18 13:04:34 +0800

    分频段和信道计算

--- mat/read_spec.m
+++ mat/read_spec.m
@@ -1,6 +1,10 @@
 %% 显示spec_mat
 data_path = 'E:\Desktop\dianci\sample_data\20210816\spec_mat\';  
-band = 1e6*[[870 880];[935 960];[1805 1840];[1840 1875];[2110 2170];[2400 2483];[2555 2655]];
+%1e6*[[870 880];[935 960];[1805 1840];[1840 1875];[2110 2170];[2400 2483];[2555 2655]];
+band = 1e6*[[870 880];[930 935];[935 954];[954 960];[1805 1830];[1830 1850];[1880 1920];[2110 2125];[2130 2145];[1850 1880];[2145 2170];[2555 2575];[2575 2635];[2635 2655];[2400 2485]];
+% band2g = 1e6*[[870 880];[930 935];[935 954];[954 960];[1710 1735];[1735 1755];[1805 1830];[1830 1850]];
+% band3g = 1e6*[[1880 1920];[1920 1935];[1940 1955];[2110 2125];[2130 2145]];
+% band4g = 1e6*[[1850 1880];[1955 1980];[2145 2170];[2555 2575];[2575 2635];[2635 2655]];
 [m, n] = size(band); %波段数和波段起止频率
 file_number = size(dir(data_path));  %路径下文件数量
 res_rmse = []; %均方根 结果存放
@@ -47,13 +51,13 @@ for j = 1:2   %遍历两个trace
     end
     %%保存
     if j == 1 
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_SR.mat'],'res_sr')
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_RMSE.mat'],'res_rmse')
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_PEAK.mat'],'res_peak')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_SR_band.mat'],'res_sr')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_RMSE_band.mat'],'res_rmse')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace1\res_PEAK_band.mat'],'res_peak')
     else
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_SR.mat'],'res_sr')
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_RMSE.mat'],'res_rmse')
-        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_PEAK.mat'],'res_peak')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_SR_band.mat'],'res_sr')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_RMSE_band.mat'],'res_rmse')
+        save(['E:\Desktop\dianci\Python_code\mat\mat_python\spec_cal_result\20210816\trace2\res_PEAK_band.mat'],'res_peak')
     end  
 end
 
@@ -87,7 +91,7 @@ name = fieldname{1};
 sr = getfield(dat, name);    %根据字段名读取数据
 [m, n] = size(sr)
 
-data_name = 'res_PEAK.mat';
+data_name = 'res_PEAK_band.mat';
 dat = load([data_path data_name]);   
 fieldname = fieldnames(dat);   %获取字段名
 name = fieldname{1};
@@ -96,20 +100,20 @@ peak = getfield(dat, name);    %根据字段名读取数据
 
 
 %% 绘制各频段结果
-aa = 20*log(peak);
+aa = peak;
 [m, n] = size(aa);
-color = ['r','g','b','k','m','c','y'];
-figure('color','w');
-for i =1:n
-    plot(aa(:,i),color(i));
-    if i==1
-        hold on;
-    end
-end
-set(gca, 'XGrid', 'on');% 显示网格
-set(gca, 'YGrid', 'on');% 显示网格
-h = legend('band1','band2','band3','band4','band5','band6','band7','northeast');
-set(h, 'FontSize', 15);
+% color = ['r','g','b','k','m','c','y'];
+% figure('color','w');
+% for i =1:n
+%     plot(aa(:,i),color(i));
+%     if i==1
+%         hold on;
+%     end
+% end
+% set(gca, 'XGrid', 'on');% 显示网格
+% set(gca, 'YGrid', 'on');% 显示网格
+% h = legend('band1','band2','band3','band4','band5','band6','band7','northeast');
+% set(h, 'FontSize', 15);
 
 %%boxplot
 figure('color','w');
@@ -118,7 +122,9 @@ temp = cell(1,m);
 for i = 1:m
     temp{i} = [num2str(band(i,1)/1e6) 'MHz to ' num2str(band(i,2)/1e6) 'MHz'];
 end
-boxplot(aa,'Notch','marker','Labels',temp,'Whisker',1)
+boxplot(20*log(aa),'Notch','marker','Labels',temp,'Whisker',1)
+set(gca, 'XGrid', 'on');% 显示网格
+set(gca, 'YGrid', 'on');% 显示网格
 %title('example');
 ylabel('E[dBmV/m]','FontSize',15);
 
@@ -141,6 +147,26 @@ set(gca, 'XGrid', 'on');% 显示网格
 set(gca, 'YGrid', 'on');% 显示网格
 h = legend('band1','band2','band3','band4','band5','band6','band7','northeast');
 set(h, 'FontSize', 15);
+
+%% 
+[m, n] = size(aa);
+
+gsm900 = sqrt(sum(aa(:,1:4).^2,2));
+dcs1800 = sqrt(sum(aa(:,5:6).^2,2));
+utms2100 = sqrt(sum(aa(:,7:9).^2,2));
+fdd_lte = sqrt(sum(aa(:,10:11).^2,2));
+tdd_lte = sqrt(sum(aa(:,11:14).^2,2));
+wifi = sqrt(sum(aa(:,15).^2,2));
+bandtable = [gsm900 dcs1800 utms2100 fdd_lte tdd_lte wifi];
+
+%%boxplot
+figure('color','w');
+temp = {'GSM900', 'DCS1800', 'UTMS2100', 'FDD LTE', 'TDD LTE', 'WIFI'};
+boxplot(20*log(bandtable),'Notch','marker','Labels',temp,'Whisker',1)
+set(gca, 'XGrid', 'on');% 显示网格
+set(gca, 'YGrid', 'on');% 显示网格
+%title('example');
+ylabel('E[dBmV/m]','FontSize',15);
 
 %% 按窗口分段计算box
 %aa = peak;
